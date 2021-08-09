6459f588f01c100a94ffb13e82f3ab3041457fda
Lalegh <53140167+Lalegh@users.noreply.github.com>
2021-08-09 10:14:21 +0800

    Add files via upload
    WHERE THE DREAM INITS.

---
+++ leetCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Solution.FindMaxConsecutiveOnes(new int[] { 1, 1, 0, 1 }));
+            //Console.WriteLine(Solution.LongestCommonPrefix(new string[] {"abab", "aba", 
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ leetCode/Solution.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    public class Solution
+    {
+        public static int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int res = 0;
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    res = 0;
+                }
+                else
+                {
+                    res += 1;
+                    sum = res > sum ? res : sum;
+                }
+            }
+            return sum;
+        }
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            string res = "";
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                if(i == 0)
+                {
+                    return "";
+                }
+                if (i == 0)
+                {
+                    res = strs[i];
+                    continue;
+                }
+                else {
+                    for (int j = 0; j < res.Length; j++)
+                    { 
+                        if (strs[i][j] != res[j])
+                        {
+                            res = res.Substring(0, j);
+                            break;
+                        }
+                    if (j + 1 == strs[i].Length)
+                    {
+
+                        res = res.Substring(0, j + 1);
+                        break;
+                    }
+
+
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+            }
+
+            }
+            return res;
+
+        }
+        public bool IsValid(string s)
+        {
+            Stack<char> res = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(' || s[i] == '[' || s[i] == '{' || s[i] == '}')
+                {
+                    res.Push(s[i]);
+                }
+                else if (s[i] == ')'||s[i] == '}'||s[i] == ']')
+                {
+                    res.Pop();
+                    
+                }
+            }
+            return res.Count == 0;
+        }
+        public static bool IsPalindrome(int x)
+        {
+            bool flag = true;
+            
+            return flag;
+        }
+    }
+}
